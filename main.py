4eb1dc6654af159a820b23355920d4aa182bd059
Ga-L <thenotorious1989@gmail.com>
2023-08-14 15:27:57 +0400

    seaborn_removed

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import os
 from streamlit import components 
-import seaborn as sns
 
 # Specify the folder where the datasets are located
 #folder_path = "datasets"
