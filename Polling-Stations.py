b2e4f61f9c54cf3903bd9a8089bb4b60c8d95b90
Ga-L <thenotorious1989@gmail.com>
2024-04-29 20:38:44 +0400

    pages name change

--- Polling-Stations.py
+++ Polling-Stations.py
@@ -15,6 +15,8 @@ st.set_page_config(
     page_icon=favicon_path  # Set the favicon
 )
 
+st.sidebar ("Select your data")
+
 # Read the SVG image as binary
 with open("logo.svg", "rb") as svg_file:
     svg_binary = svg_file.read()
