dd13b6cdf4fcda4ca49b02dd78540f918388958e
Ga-L <thenotorious1989@gmail.com>
2024-04-29 21:12:01 +0400

    header visibility

--- Polling-Stations.py
+++ Polling-Stations.py
@@ -23,6 +23,7 @@ hide_st_style = """
             header {visibility: hidden;}
             </style>
             """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
 # Read the SVG image as binary
 with open("logo.svg", "rb") as svg_file:
