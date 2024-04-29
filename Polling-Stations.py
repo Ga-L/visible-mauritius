bb4a321b797b6c5f8e61eb19d39c564491b87d4c
Ga-L <thenotorious1989@gmail.com>
2024-04-29 21:55:10 +0400

    revert action

--- Polling-Stations.py
+++ Polling-Stations.py
@@ -25,8 +25,6 @@ hide_st_style = """
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
-st.sidebar.success("Select a page")
-
 # Read the SVG image as binary
 with open("logo.svg", "rb") as svg_file:
     svg_binary = svg_file.read()
